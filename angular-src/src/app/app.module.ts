2a9589a0f1fe4e15cf936dd8e6de86f6d0a11557
Arianit Hetemi <nitihetemi@gmail.com>
2018-05-21 09:54:24 +0200

    MEAN boilerplate

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+// DB connection
+mongoose.connect(config.database);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database!");
+});
+
+const app = express();
+
+// Port number
+const port = 3000;
+
+// CORS middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+// Index Route
+app.get("/", (req, res) => {
+  res.send("Message");
+});
+
+app.listen(port, () => {
+  console.log("Server started on port:" + port);
+});
