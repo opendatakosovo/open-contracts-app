29e5ca163477f3684d85a3dc49b052cefbf3fc44
Dorron Zherka <dorron.zherka@gmail.com>
2018-05-30 13:30:18 +0200

    Change front-end to library to md bootstrap

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -20,6 +20,7 @@ import { LayoutComponent } from './layouts/main-page-layout/layout/layout.compon
 import { DashboardLayoutComponent } from './layouts/dashboard-page-layout/dashboard-layout/dashboard-layout.component';
 import { DashboardHeaderComponent } from './layouts/dashboard-page-layout/dashboard-header/dashboard-header.component';
 import { DashboardFooterComponent } from './layouts/dashboard-page-layout/dashboard-footer/dashboard-footer.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 
@@ -69,7 +70,10 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    NgbModule.forRoot()
+    MDBBootstrapModule.forRoot()
+  ],
+  schemas:[
+    NO_ERRORS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
