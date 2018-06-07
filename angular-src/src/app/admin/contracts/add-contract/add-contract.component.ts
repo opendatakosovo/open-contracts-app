ee555e39f0479849491f350434ea3c18ce150956
leobaz <leutrim.kosumi@hotmail.com>
2018-06-07 13:20:25 +0200

    Implement contracts validation

--- angular-src/src/app/admin/contracts/add-contract/add-contract.component.ts
+++ angular-src/src/app/admin/contracts/add-contract/add-contract.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Contract } from '../../../../../../models/contract';
 
 @Component({
   selector: 'app-add-contract',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContractComponent implements OnInit {
 
+  @Input('contract') contract: Contract;
+  
+
   constructor() { }
 
   ngOnInit() {
+    let activityTitle = this.contract.activityTitle;
+    this.contract.publicationDate = '';
+    this.contract.noOfDownloads = '';
+    this.contract.noOfOffers = '';
+    this.contract.dateOfGivenContractPublication = '';
+    this.contract.dateOfNoticeCancellations = '';
+    this.contract.nameOfOE = '';
+    this.contract.signingDate = '';
+    this.contract.startDateOfImplemetation = '';
+    this.contract.contractClosingDate = '';
+    this.contract.predictedContractAmount = '';
+    this.contract.totalAmount = '';
   }
 
 }
---
+++ models/contract.ts
@@ -0,0 +1,14 @@
+export interface Contract {
+    activityTitle: string,
+    publicationDate: string,
+    noOfDownloads: string,
+    noOfOffers: string,
+    dateOfGivenContractPublication: string,
+    dateOfNoticeCancellations: string,
+    nameOfOE: string,
+    signingDate: string,
+    startDateOfImplemetation: string,
+    contractClosingDate: string,
+    predictedContractAmount: string,
+    totalAmount: string
+}
