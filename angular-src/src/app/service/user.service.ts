ba1fe84a7646a1608e7296f6bd94d84fe2e651f8
Dorron Zherka <dorron.zherka@gmail.com>
2018-06-11 16:27:37 +0200

    Add to environments api url and improve modal of registration of user

--- angular-src/src/app/service/user.service.ts
+++ angular-src/src/app/service/user.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import "rxjs/add/operator/map";
+import {environment} from '../../environments/environment';
+
 
 @Injectable()
 export class UserService {
+    
 
   constructor(public http : Http) { }
 
 
   getUsers() {
-    return this.http.get('http://localhost:3000/user').map(res => res.json().users);
+    return this.http.get(`${environment.apiUrl}/user`).map(res => res.json());
+  }
+  addUser(user){
+    return this.http.post(`${environment.apiUrl}/user`,user).map(res => res.json());
   }
 }
