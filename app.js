0ee800c2380651cb3a0b38766e339960d85ade41
Dorron Zherka <dorron.zherka@gmail.com>
2018-06-30 15:45:04 +0200

    Modify CORS

--- app.js
+++ app.js
@@ -28,16 +28,8 @@ const port = 3000;
 const validator = require('express-validator');
 app.use(validator());
 
-
-
 // CORS middleware
-// app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -56,6 +48,7 @@ app.use(morgan('dev'));
 // Registering all controllers
 app.use(require('./controllers'));
 
+// Route all upload files
 app.get('/uploads/:filename', (req, res) => {
   res.sendFile(path.join(__dirname, `uploads/${req.params.filename}`));
 })
