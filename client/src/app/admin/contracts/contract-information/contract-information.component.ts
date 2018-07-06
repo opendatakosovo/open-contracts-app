743b6391809789b10404ea13ec834c0d451be622
leobaz <leutrim.kosumi@hotmail.com>
2018-07-06 15:54:01 +0200

    Implement view contract form

--- client/src/app/admin/contracts/contract-information/contract-information.component.ts
+++ client/src/app/admin/contracts/contract-information/contract-information.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Contract } from '../../../models/contract';
+import { ContractsService } from '../../../service/contracts.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-contract-information',
@@ -6,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-information.component.css']
 })
 export class ContractInformationComponent implements OnInit {
+  contracts: Contract[];
+  contract: Contract;
 
-  constructor() { }
+  constructor(public contractsService: ContractsService, private router: ActivatedRoute) {
+    this.contract = new Contract();
+    const id = this.router.snapshot.paramMap.get('id');
+    this.contractsService.getContractByID(id).subscribe(data => {
+      this.contract = data;
+    });
+    console.log(this.contract.contract.annexes.length);
+  }
 
   ngOnInit() {
   }
