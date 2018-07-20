b9c9f5e3a90a8a0e78eaeb292a76f768f6f81d5a
leobaz <leutrim.kosumi@hotmail.com>
2018-07-20 13:16:31 +0200

    Implement about-us multilanguage feature

--- client/src/app/main/about-us/about-us.component.ts
+++ client/src/app/main/about-us/about-us.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class AboutUsComponent implements OnInit {
 
 
-    constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('sq');
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
 
 }
