250334b9a887d4b2f26049baf4f7467be2b94516
leobaz <leutrim.kosumi@hotmail.com>
2018-07-23 18:02:53 +0200

    Improve contracts-list

--- client/src/assets/js/sb-admin.js
+++ client/src/assets/js/sb-admin.js
@@ -48,15 +48,6 @@
       }, 1000, 'easeInOutExpo');
       event.preventDefault();
     });
-    $('.datatable-header').css({ 'color': 'white', 'background-color': '#32a6bd' });
-    setTimeout(function () {
-      $('.datatable-body-row').css({ 'color': '#5f5e5e', 'border': 'none' });
-      $('.datatable-body-cell').css('margin-right', '27px');
-      $('.ngx-datatable.bootstrap .datatable-body .datatable-body-row').css('width', '1815px');
-      $('.ngx-datatable.bootstrap').css('border-radius', '10px');
-      $('.ngx-datatable.bootstrap .datatable-body .datatable-body-row .datatable-body-cell').css('padding-left', '14px');
-      $('.ngx-datatable.bootstrap .datatable-header .datatable-header-cell').css('padding-left', '26px');
-    }, 300);
 
   });
 })(jQuery); // End of use strict
