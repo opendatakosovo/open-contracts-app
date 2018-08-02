46ffee08adb97148c31d229b091eac619d3268f0
Arianit Hetemi <nitihetemi@gmail.com>
2018-08-02 11:56:11 +0200

    Implement User Data API in dashboard

--- client/src/app/admin/dashboard/dashboard.component.ts
+++ client/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
+import { DataService } from '../../service/data.service';
+import { Chart } from 'angular-highcharts';
+import { UserData } from './userData';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +11,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  private unsubscribeAll: Subject<any> = new Subject<any>();
+  userData: UserData;
+  usersByDirectoratesChart: Chart;
+  usersByRolesChart: Chart;
 
-  constructor() { }
+  constructor(public dataService: DataService) {
+    this.userData = new UserData();
+  }
 
   ngOnInit() {
+    this.dataService.getUserData()
+      .takeUntil(this.unsubscribeAll)
+      .subscribe(userDataRes => {
+        this.userData = userDataRes;
+
+        this.renderUsersByDirectoratesChart(this.userData.totalUsersWithoutDirectorate, this.userData.totalUsersWithDirectorate);
+        this.renderUsersByRoleChart(this.userData.totalAdminUsers, this.userData.totalSimpleUsers);
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  renderUsersByDirectoratesChart(totalUsersWithoutDirectorate: number, totalUsersWithDirectorate: number): void {
+    this.usersByDirectoratesChart = new Chart({
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Numri i përdoruesve të thjeshtë aktiv në bazë të drejtorive'
+      },
+      xAxis: {
+        type: 'category'
+      },
+      yAxis: {
+        title: {
+          text: 'Numri i përdoruesve'
+        }
+      },
+      series: [{
+        name: 'Numri',
+        data: [{
+            name: 'Pa drejtori',
+            y: totalUsersWithoutDirectorate
+          }, {
+            name: 'Me drejtori',
+            y: totalUsersWithDirectorate
+          }
+        ]
+      }]
+    });
+  }
+
+  renderUsersByRoleChart(totalAdminUsers: number, totalSimpleUsers: number): void {
+    this.usersByRolesChart = new Chart({
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Numri i përdoruesve në bazë të roleve'
+      },
+      xAxis: {
+        type: 'category'
+      },
+      yAxis: {
+        title: {
+          text: 'Numri i përdoruesve'
+        }
+      },
+      series: [{
+        name: 'Numri',
+        data: [{
+          name: 'Administratorë',
+          y: totalAdminUsers
+        }, {
+          name: 'Të thjeshtë',
+          y: totalSimpleUsers
+        }]
+      }]
+    });
   }
 
 }
