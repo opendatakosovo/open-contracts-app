aaee91ebdfd30c079f566865bb26b08585ab4ad4
leobaz <leutrim.kosumi@hotmail.com>
2018-08-10 16:29:57 +0200

    Fix title in the admin side to albanian

--- client/src/app/layouts/dashboard-page-layout/dashboard-layout/dashboard-layout.component.ts
+++ client/src/app/layouts/dashboard-page-layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../service/user.service';
 import { Router } from '@angular/router';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { Subject } from 'rxjs/Subject';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -8,11 +11,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard-layout.component.css']
 })
 export class DashboardLayoutComponent implements OnInit {
-
+  private unsubscribeAll: Subject<any> = new Subject<any>();
   constructor(
     private userService: UserService,
-    private router: Router,
-  ) { }
+    private router: Router, private translate: TranslateService, private titleService: Title
+  ) {
+    this.translate.get('pageTitle.title')
+      .takeUntil(this.unsubscribeAll)
+      .subscribe(name => {
+        this.titleService.setTitle(name);
+      });
+  }
 
   ngOnInit() {
   }
