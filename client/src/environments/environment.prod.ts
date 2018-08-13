0c146271715afa7c6caee3331f97819a7b152a81
Dorron Zherka <dorron.zherka@gmail.com>
2018-08-13 21:06:06 +0200

    Hotfix for prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Api url
-  apiUrl: 'http://kontratatehapura.prishtinaonline.com/'
+  apiUrl: 'http://kontratatehapura.prishtinaonline.com'
 };
